76d4cf95fd05b2e377b5affbfaf62974ea9a0473
Olivia Cheung <84092184+OCheung313@users.noreply.github.com>
2022-05-26 14:17:59 -0500

    Took out personal school number

---
+++ Order.java
@@ -0,0 +1,26 @@
+//Olivia Cheung
+
+
+package PaneraCheese;
+
+import java.text.SimpleDateFormat;
+
+public class Order {
+	
+	protected String Name;
+	protected String Order;
+	protected String TimeStamp;
+	
+	public Order() {
+		
+	}
+	
+	public Order(String name, String order, String time){
+		
+		Name = name; 
+		Order = order;
+		TimeStamp = time;
+		
+	}
+
+}
